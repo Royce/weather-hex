0a4f683a3e49ee8d034d061eab9ab2bd99ce8ca5
Royce Townsend <git@roycetownsend.com>
2020-11-28 16:22:13 +0800

    Problem: Weather mixing still a little hokey.
    Add more tests and try a new implementation.

--- src/weather/mix.test.ts
+++ src/weather/mix.test.ts
@@ -4,7 +4,7 @@ import { Weather } from "./options";
 const description = ({ temperature, wind, sky, water }: Weather) =>
   `${water.join(", ")}: ${temperature}, ${wind}, ${sky}`;
 
-test("mix to: heavy rain, lightning: warm, breeze, overcast", () => {
+test("mix to: rain, lightning: warm, breeze, overcast", () => {
   const result = mixes(
     {
       temperature: "hot",
@@ -17,10 +17,81 @@ test("mix to: heavy rain, lightning: warm, breeze, overcast", () => {
 
   expect(result.map(description)).toMatchInlineSnapshot(`
     Array [
-      "heavy rain, lightning: warm, breeze, overcast",
-      "heavy rain: warm, breeze, overcast",
       "light rain, lightning: warm, breeze, overcast",
       "light rain: warm, breeze, overcast",
+    ]
+  `);
+});
+
+test("mix to light snow", () => {
+  const result = mixes(
+    {
+      temperature: "freezing",
+      sky: "overcast",
+      wind: "gale",
+      water: ["heavy snowfall"],
+    },
+    { temperature: "cool", sky: "overcast", wind: "calm", water: ["fog"] }
+  );
+
+  expect(result.map(description)).toMatchInlineSnapshot(`
+    Array [
+      "light snowfall: freezing, breeze, overcast",
+    ]
+  `);
+});
+
+test("light-rain+ / light-rain+", () => {
+  const result = mixes(
+    {
+      temperature: "cool",
+      sky: "overcast",
+      wind: "calm",
+      water: ["light rain", "fog"],
+    },
+    {
+      temperature: "cool",
+      sky: "overcast",
+      wind: "breeze",
+      water: ["light rain", "lightning"],
+    }
+  );
+
+  // I expect that there is nothing weird like HAIL.
+  // It should be rain or fog or lightning or nothing.
+  expect(result.map(description)).toMatchInlineSnapshot(`
+    Array [
+      "light rain: cool, calm, overcast",
+      "fog: cool, calm, overcast",
+      "dry: cool, calm, overcast",
+      "light rain: cool, breeze, overcast",
+      "lightning: cool, breeze, overcast",
+      "dry: cool, breeze, overcast",
+    ]
+  `);
+});
+
+test("lightning storm  mix  clear skies", () => {
+  const result = mixes(
+    {
+      temperature: "cool",
+      sky: "clear",
+      wind: "calm",
+      water: ["dry"],
+    },
+    {
+      temperature: "cool",
+      sky: "overcast",
+      wind: "gale",
+      water: ["heavy rain", "lightning"],
+    }
+  );
+
+  // Light rain!
+  // No lightning, because not overcast.
+  expect(result.map(description)).toMatchInlineSnapshot(`
+    Array [
+      "light rain: cool, breeze, light clouds",
     ]
   `);
 });
--- src/weather/mix.ts
+++ src/weather/mix.ts
@@ -15,34 +15,63 @@ export function mix(a: Weather, b: Weather): Weather {
   return _.sample(options) as Weather;
 }
 
-const adjacentWater: { [K in Water]?: Water[] } = {
+const adjacentWater: { [K in Water]: Water[] } = {
   "heavy rain": ["heavy rain", "light rain"],
   "light rain": _.without(waterList, "heavy snowfall", "light snowfall"),
   "heavy snowfall": ["heavy snowfall", "light snowfall"],
-  "light snowfall": _.without(waterList, "heavy rain", "light rain"),
+  "light snowfall": _.without(
+    waterList,
+    "heavy rain",
+    "light rain",
+    "lightning"
+  ),
   dry: _.without(waterList, "heavy snowfall", "heavy rain"),
   lightning: ["lightning", "dry"],
   hail: ["hail", "dry"],
   fog: ["fog", "dry"],
 };
 
+type Pair<T> = [T, T];
+
+function expandWaterCombo([w1, w2]: Pair<Water | undefined>) {
+  const list: Water[] =
+    !w1 && !w2
+      ? ["dry"]
+      : w1 === w2
+      ? [w1 as Water]
+      : (w1 === "dry" && !w2) || (w2 === "dry" && !w1)
+      ? ["dry", "light rain", "light snowfall"]
+      : _.intersection(adjacentWater[w1 || "dry"], adjacentWater[w2 || "dry"]);
+
+  console.log(`**${i + 1000}`, [w1, w2], list);
+  return list;
+}
+
+var i = 0;
 export function mixes(a: Weather, b: Weather): Weather[] {
+  i++;
+
+  const combo: Pair<Pair<Water | undefined>>[] = [
+    [
+      [a.water[0], b.water[0]],
+      [a.water[1], b.water[1]],
+    ],
+    [
+      [a.water[0], b.water[1]],
+      [b.water[0], a.water[1]],
+    ],
+  ];
+
   var x: [Water, Water][] = [];
-
-  const [a1, a2] = a.water.map((w) => adjacentWater[w] || waterList);
-  const [b1, b2] = b.water.map((w) => adjacentWater[w] || waterList);
-
-  for (const w1 of _.intersection(a1, b1)) {
-    for (const w2 of a2 && b2 ? _.intersection(a2, b2) : a2 || b2 || ["dry"]) {
-      x.push(_.sortBy([w1, w2]) as [Water, Water]);
+  for (const [p1, p2] of combo) {
+    for (const w1 of expandWaterCombo(p1)) {
+      for (const w2 of expandWaterCombo(p2)) {
+        x.push(_.sortBy([w1, w2]) as [Water, Water]);
+      }
     }
   }
 
-  for (const w1 of b2 ? _.intersection(a1, b2) : a1) {
-    for (const w2 of a2 ? _.intersection(a2, b1) : b1) {
-      x.push(_.sortBy([w1, w2]) as [Water, Water]);
-    }
-  }
+  console.log(`**${i + 1000}`, x);
 
   const waterPairs = _.chain(x)
     .map<WaterGroup>(([item1, item2]) =>
